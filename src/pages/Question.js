fa00098b41e7bceea41cc0f4167d4501364fe4a3
ohh31 <saewon412@gmail.com>
2021-05-06 23:07:30 +0900

    [ADD] 질문 페이지 파일 생성

---
+++ src/pages/Question.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Route, Link } from 'react-router-dom';
+import '../App.css';
+
+class Question extends React.Component {
+    render() {
+      return <div class = "app">
+      <div class = "bg">  
+      </div>
+      </div>
+    }
+}
+
+export default Question;
