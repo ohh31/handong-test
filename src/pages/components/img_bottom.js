a567a17289b62e5d7b7efba40e08e945dd41080a
ohh31 <saewon412@gmail.com>
2021-05-10 16:47:42 +0900

    refactor: bottom image 컴포넌트화

---
+++ src/pages/components/img_bottom.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Route, Link } from 'react-router-dom';
+
+function ImgBottom ({imgPath}){
+    const bottomStyle = {
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "center",
+        verticalAlign:"bottom"
+    }
+    const imgStyle = {
+    width : "700px",
+    height :"450px",
+    margin: "0px",
+    backgroundSize: "cover"
+      }
+
+    return (
+        <div class = "content-bottom" style = {bottomStyle}>
+        <img style = {imgStyle} src = {String(imgPath)}></img></div>
+    );
+}
+
+export default ImgBottom;
