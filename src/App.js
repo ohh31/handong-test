0105edbc6a400b2cb0f7131ee12f7ae3bcc096b2
ohh31 <saewon412@gmail.com>
2021-05-12 02:54:09 +0900

    refactor : class를 function으로 변경

--- src/App.js
+++ src/App.js
@@ -4,16 +4,16 @@ import Intro from "./pages/Intro";
 import Desc from "./pages/Desc";
 import Question from "./pages/Question";
 
-class App extends React.Component {
-  render() {
+function App () {
    return (
+     <>
     <HashRouter>
       <Route path="/" component={Intro} />
       <Route path="/desc" component={Desc} />
       <Route path="/question" component={Question} />
     </HashRouter>
+    </>
   );
-}
 }
 
 export default App;
--- src/pages/Intro.js
+++ src/pages/Intro.js
@@ -4,19 +4,17 @@ import '../App.css';
 import Background from "./components/background.js"
 import ContentBtn from "./components/buttons/content_btn.js"
 import ContentBottom from "./components/img_bottom.js"
-class Intro extends React.Component {
-    render() {
-      return <Background>
-          <div class = "content-title">
-            <span>한동에서<br></br>나는 </span> 
-            <span style = {{color: '#FFA200'}} class = "font yellow">어떤 유형</span>
-            <span>일까 ?</span>
-          </div>
-          <ContentBtn value = "테스트 시작" path ="/question"></ContentBtn>
-        <ContentBottom imgPath="/images/bg-image.png"></ContentBottom>
-        </Background>
 
+  function Intro() {
+    return <Background>
+    <div class = "content-title">
+      <span>한동에서<br></br>나는 </span> 
+      <span style = {{color: '#FFA200'}} class = "font yellow">어떤 유형</span>
+      <span>일까 ?</span>
+    </div>
+    <ContentBtn value = "테스트 시작" path ="/question"></ContentBtn>
+  <ContentBottom imgPath="/images/bg-image.png"></ContentBottom>
+  </Background>
   }
-  }
-  
+
   export default Intro;
