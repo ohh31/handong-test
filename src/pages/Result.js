1f3deb5117290f87888d122b7b28a7aded9d040a
ohh31 <saewon412@gmail.com>
2021-05-19 18:36:14 +0900

    feat : result 페이지 추가

---
+++ src/pages/Result.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Background from "./components/background.js"
+
+
+function Result() {
+    return <Background>
+    {/* <CSSTransition
+         in={true}
+         appear = {true}
+         timeout={300}
+         classNames="fade"
+       >
+    </CSSTransition>  */}
+    </Background>
+}
+
+export default Result;
