f48b855e70d9c5b6e0a23807ee4d9d8f2ce33816
ohh31 <saewon412@gmail.com>
2021-05-19 18:34:36 +0900

    feat : router 추가

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React from "react";
-import { HashRouter, Route } from "react-router-dom";
+import { Route } from "react-router-dom";
 import Intro from "./pages/Intro";
 import Desc from "./pages/Desc";
 import Question from "./pages/Question";
+import Result from "./pages/Result";
 
 function App () {
    return (
-     <>
-    <HashRouter>
-      <Route path="/" component={Intro} />
-      <Route path="/desc" component={Desc} />
-      <Route path="/question" component={Question} />
-    </HashRouter>
-    </>
+     <div>
+    <Route path="/" component={Intro} />
+    <Route path="/desc" component={Desc} />
+    <Route path="/question" component={Question} />
+    <Route path="/result" component={Result} />
+    </div>
   );
 }
+
 
 export default App;
