ed4c69a07bff09e4f703539a21cd5b9d6596d835
ohh31 <saewon412@gmail.com>
2021-05-20 22:37:23 +0900

    feat : button 선택값 관리 추가

--- src/pages/Question.js
+++ src/pages/Question.js
@@ -4,55 +4,77 @@ import '../App.css';
 import SelectBtn from './components/buttons/select_btn.js';
 import Background from "./components/background.js"
 import { CSSTransition } from 'react-transition-group';
+import {useHistory} from "react-router";
 
-const duration = 300;
+const qstDataSet = [
+  {
+      id : 1,
+      type : "energy",
+      question: "1번 질문입니다",
+      ans1: "대답 1번 입니다",
+      ans2: "대답 2번 입니다",
+  },
+  {
+      id : 2,
+      type : "info",
+      question: "2번 질문입니다",
+      ans1: "대답 1번 입니다",
+      ans2: "대답 2번 입니다",
+  },
+  {
+      id : 3,
+      type : "decide",
+      question: "3번 질문입니다",
+      ans1: "대답 1번 입니다",
+      ans2: "대답 2번 입니다",
+  },
+  {
+      id : 4,
+      type : "life",
+      question: "4번 질문입니다",
+      ans1: "대답 1번 입니다",
+      ans2: "대답 2번 입니다",
+  },
+  {
+      id : 5,
+      type : "life",
+      question: "5번 질문입니다",
+      ans1: "대답 1번 입니다",
+      ans2: "대답 2번 입니다",
+  },
+]
 
-
-function Question({history}){
+function Question(){
   
   const [data, setData] = useState(0);
   const [showQst, setShowQst] = useState(true);
-  const qstDataSet = [
-    {
-        id : 1,
-        question: "1번 질문입니다",
-        ans1: "대답 1번 입니다",
-        ans2: "대답 2번 입니다",
-    },
-    {
-        id : 2,
-        question: "2번 질문입니다",
-        ans1: "대답 1번 입니다",
-        ans2: "대답 2번 입니다",
-    },
-    {
-        id : 3,
-        question: "3번 질문입니다",
-        ans1: "대답 1번 입니다",
-        ans2: "대답 2번 입니다",
-    },
-    {
-        id : 4,
-        question: "4번 질문입니다",
-        ans1: "대답 1번 입니다",
-        ans2: "대답 2번 입니다",
-    },
-    {
-        id : 5,
-        question: "5번 질문입니다",
-        ans1: "대답 1번 입니다",
-        ans2: "대답 2번 입니다",
-    },
-]
+  const history = useHistory();
+  const [state, setState] = useState({
+    energy: 0,
+    info: 0,
+    decide : 0,
+    life : 0,
+  });
 
-  const onChanges = e => {
+  const handleOnSubmit = (event) => {
+    event.preventDefault();
+        history.push({
+      pathname:`/result/${data}`,
+      state
+    });
+  };
+
+  const handleInputChange = (event) => {
+    event.preventDefault();
+    const value = event.target.id.split("-");
+     const type = value[0];
+     const ans = value[1];
+     setState((prevState) => ({
+      ...prevState,
+      [type]: ans === "ans1" ? prevState[type] + 1 : prevState[type] - 1
+         }));
     setData(prevNumber => prevNumber + 1);
-    console.log(data);
-  }
-
-  const toEnd = e => {
-    console.log(data);
-  }
+  };
 
   return <Background>
    <CSSTransition
@@ -65,15 +87,12 @@ function Question({history}){
 <div class = "question-text">
 {qstDataSet[data].question}
 </div>
-<SelectBtn value = {qstDataSet[data].ans1} onChange = {qstDataSet.length-1 === data ? history.push('/result') : onChanges}></SelectBtn>
-<SelectBtn value = {qstDataSet[data].ans2} onChange = {qstDataSet.length-1 === data ? history.push('/result') : onChanges}></SelectBtn>
+<SelectBtn id = {qstDataSet[data].type + '-ans1'} value = {qstDataSet[data].ans1} onChange = {qstDataSet.length-1 === data ? handleOnSubmit : handleInputChange}></SelectBtn>
+<SelectBtn id = {qstDataSet[data].type + '-ans2'} value = {qstDataSet[data].ans2} onChange = {qstDataSet.length-1 === data ? handleOnSubmit : handleInputChange}></SelectBtn>
 </div>
 </CSSTransition>
   </Background>
 } 
 
-{/* <Link to="/users">
-          <button>Users</button>
-        </Link> */}
 export default Question;
 
