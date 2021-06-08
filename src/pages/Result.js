54af96153e38abd1cea937447fc5d79e571f5587
ohh31 <saewon412@gmail.com>
2021-06-09 05:25:03 +0900

    feat : result값 firebase 연동

--- src/pages/Result.js
+++ src/pages/Result.js
@@ -4,29 +4,39 @@ import {useLocation} from 'react-router';
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 import { firestore } from '../firebase';
+import firebase from 'firebase/app';
 
 
 function Result() {
     let location = useLocation();
     const [finalResult, setfinalResult] = useState('');
+    let ref = firestore.collection('Result');
+
+
+    function increaseTotalCount(){
+        ref.doc(`fF2LVDgqIHKIjKYbrU6s`).update({
+            totalCount : firebase.firestore.FieldValue.increment(1)
+          });
+    }
+
+    function saveResultType(result){
+        var fieldName = `type.`+ result;
+        console.log(fieldName);
+        ref.doc(`fF2LVDgqIHKIjKYbrU6s`).update({
+            [`${fieldName}`]: firebase.firestore.FieldValue.increment(1)
+          });
+    }
 
     useEffect(() => {
         const result = location.state.result;
         setfinalResult(result);
-        
+        saveResultType(result);
+        increaseTotalCount();
       }, []);
 
-    function onCall(){
-        let ref = firestore.collection('Result');
-        ref.doc('fF2LVDgqIHKIjKYbrU6s').get().then(doc => {
-            console.log(doc.data().totalCount);
-        })
-      };
     
     return <Background>
-        <h3>{finalResult}</h3>
-        <button onClick={onCall}>가져오기</button>
-      
+        <h3>{finalResult}</h3>      
     </Background>
 }
 
