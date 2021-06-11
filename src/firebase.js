4db2efe80694c62769cd97cc42cd53b0e0fbe39d
ohh31 <saewon412@gmail.com>
2021-06-12 02:13:09 +0900

    fix : firestore 함수 형식으로 맞춤

--- src/firebase.js
+++ src/firebase.js
@@ -15,5 +15,5 @@ const firebaseConfig = {
   };
   
 firebase.initializeApp(firebaseConfig);
-const firestore = new firebase.firestore;
+const firestore = new firebase.firestore();
 export { firestore };
