ba2f62510d0850da19651ab0c5bc2ab021a6c3cb
ohh31 <saewon412@gmail.com>
2021-06-13 20:12:32 +0900

    fix : loading page style 수정

--- src/pages/Loading.js
+++ src/pages/Loading.js
@@ -5,7 +5,11 @@ import { CSSTransition } from 'react-transition-group';
 function Loading(){
 
     const [showQst, setShowQst] = useState(true);
-
+    const style = {
+        fontSize : "15px",
+        fontWeight: "bold",
+        paddingTop : "5px"
+    }
 return  <CSSTransition
 in={showQst}
 timeout={300}
@@ -16,7 +20,7 @@ classNames="slide-in"
     <div className = "loading-item"></div>
     <div className = "loading-item"></div>
 </div>
-<p>결과 분석 중</p>
+<p style ={style}>결과 분석 중</p>
 </div></CSSTransition>
 }
 
