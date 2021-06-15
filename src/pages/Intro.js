79adc532a54287384c38e6652b308ebdc1643c1c
ohh31 <saewon412@gmail.com>
2021-06-15 23:54:01 +0900

    fix : boxstyle 수정

--- src/pages/Intro.js
+++ src/pages/Intro.js
@@ -56,8 +56,10 @@ const fontstyles = {
 }
   
 const boxStyle = {
-  textAlign : "center",
-  alignSelf :"center"
+  display : "flex",
+  flexDirection:"column",
+  justifyContent: "center",
+  alignItems : "center",
 }
 
     return <Background>  
@@ -74,7 +76,7 @@ const boxStyle = {
      <button className="content-btn" onClick ={()=> setIsVisible(false)}>
           <span className = "btn-text" >테스트 시작</span>
         </button>
-        <span className = "content-desc-text"><br></br>한동인 {totalCount}명이 테스트 하였습니다</span>
+        <span className = "content-desc-text">한동인 {totalCount}명이 테스트 하였습니다</span>
         </div> :         <p style = {fontstyles}>단시간 많은 접속량으로 인해 <br></br>서버가 일일 사용량 한도를 초과하였습니다.<br></br>태평양 표준시 자정에 맞춰 17시에 복구될 예정입니다. <br></br>많은 관심 감사드립니다! :)</p>
 
         }
