5e7fecc04718a844378ba396c73c061d0c4390dc
ohh31 <saewon412@gmail.com>
2021-06-15 20:19:12 +0900

    fix : 첫 로드 시 server 중단 시 안내 문자 나타나는 문제 해결

--- src/pages/Intro.js
+++ src/pages/Intro.js
@@ -13,12 +13,16 @@ function Intro() {
   const [totalCount, setTotalCount]=useState(0);
   const history = useHistory();
   let ref = firestore.collection('Result');
+  const [isUpdated , setUpdated] = useState(false);
+  const [error , setError] = useState(false);
 
-//  console.log("upload");
  
  useEffect(
   () => { 
+    if(isUpdated === false){
     getTotalCount();
+    setUpdated(true);
+    }
   },
 );
 
@@ -40,7 +44,8 @@ function getTotalCount(){
         console.log("No such data");
     }
 }).catch((error) => {
-    console.log("Error getting document:", error);
+    setError(true);
+    // console.log("Error getting document:", error);
 });
 }
 
@@ -52,6 +57,7 @@ const fontstyles = {
   
 const boxStyle = {
   textAlign : "center",
+  alignSelf :"center"
 }
 
     return <Background>  
@@ -63,14 +69,14 @@ const boxStyle = {
     unmountOnExit
     ><div className = "content-container">
     <ContentTitle/> 
-    {totalCount === 0 ? 
-        <p style = {fontstyles}>단시간 많은 접속량으로 인해 <br></br>서버가 일일 사용량 한도를 초과하였습니다.<br></br>태평양 표준시 자정에 맞춰 17시에 복구될 예정입니다. <br></br>많은 관심 감사드립니다! :)</p>
-        :<div style = {boxStyle}>
+    {error === false ? 
+        <div style = {boxStyle}>
      <button className="content-btn" onClick ={()=> setIsVisible(false)}>
           <span className = "btn-text" >테스트 시작</span>
         </button>
         <span className = "content-desc-text"><br></br>한동인 {totalCount}명이 테스트 하였습니다</span>
-        </div>
+        </div> :         <p style = {fontstyles}>단시간 많은 접속량으로 인해 <br></br>서버가 일일 사용량 한도를 초과하였습니다.<br></br>태평양 표준시 자정에 맞춰 17시에 복구될 예정입니다. <br></br>많은 관심 감사드립니다! :)</p>
+
         }
         </div>
        </CSSTransition>
